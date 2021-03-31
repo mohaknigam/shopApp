111a5901f16ce135e2660423ade949851a4477b2
mohak <mohaknigam20@gmail.com>
2021-03-31 14:51:56 +0530

    grid setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/screens/productOverviewScreen.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,21 +9,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'MyShop',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('MyShop'),
-      ),
-      body: Center(
-        child: Text('Hii'),
-      ),
+      home: ProductOverviewScreen(),
     );
   }
 }
